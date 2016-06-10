0e339a15cada56f23f7b9b18f86f73d7aca2ecd3
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-06-10 17:29:56 +0100

    Added data attribute to scripts

---
+++ modules/sourcepoint_dfp/theme/dfp_tag__sourcepoint.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Default template for dfp tags.
+ */
+?>
+<div <?php print drupal_attributes($placeholder_attributes) ?>>
+  <?php if (isset($slug)):
+    print drupal_render($slug);
+  endif; ?>
+  <script type="text/javascript"<?php print drupal_attributes($sourcepoint_attributes); ?>>
+    googletag.cmd.push(function() {
+      googletag.display("<?php print $tag->placeholder_id ?>");
+    });
+  </script>
+</div>

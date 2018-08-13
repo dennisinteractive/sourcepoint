8f964529f650ee53f297e3fa03d78d8e3f65f561
David Grayston <david_grayston@dennis.co.uk>
2018-08-13 19:17:51 +0100

    Issue #2992302 CMP overlay

---
+++ templates/sourcepoint-cmp-overlay.tpl.php
@@ -0,0 +1,5 @@
+<div id="sourcepoint-cmp-modal" class="sourcepoint-cmp-modal">
+  <div class="sourcepoint-cmp-modal-content" style="height: <?php print $height; ?>; width:<?php print $width; ?>;">
+    <iframe style="height: <?php print $height; ?>; width:<?php print $width; ?>;" src="<?php print $url; ?>"></iframe>
+  </div>
+</div>

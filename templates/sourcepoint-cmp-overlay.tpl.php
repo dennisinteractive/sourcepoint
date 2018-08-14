acd0bfb2b6a3c736c7834cd6c31a01e6f74cdf77
David Grayston <david_grayston@dennis.co.uk>
2018-08-14 01:11:09 +0100

    Issue #2992302 updated template to work on smaller screens

--- templates/sourcepoint-cmp-overlay.tpl.php
+++ templates/sourcepoint-cmp-overlay.tpl.php
@@ -1,5 +1,5 @@
 <div id="sourcepoint-cmp-modal" class="sourcepoint-cmp-modal">
-  <div class="sourcepoint-cmp-modal-content" style="height: <?php print $height; ?>; width:<?php print $width; ?>;">
-    <iframe id="sourcepoint-cmp-modal-iframe" style="height: <?php print $height; ?>; width:<?php print $width; ?>;" data-src="<?php print $url; ?>"></iframe>
+  <div class="sourcepoint-cmp-modal-content" style="height: <?php print $height; ?>; width:auto; max-width:<?php print $width; ?>;">
+    <iframe id="sourcepoint-cmp-modal-iframe" style="height: <?php print $height; ?>; width:100%;" data-src="<?php print $url; ?>"></iframe>
   </div>
 </div>
